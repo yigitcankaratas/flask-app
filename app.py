f0646903fa57f6a1519e48f4e563905d0e309c5d
Yiğitcan Karataş <64430352+yigitcankaratas@users.noreply.github.com>
2025-11-17 11:15:36 +0300

    Fix greeting message in Flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask + Jenkins + Kubernetes...!"
+    return "Hello from Flask + Jenkins + Kubernetes!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
